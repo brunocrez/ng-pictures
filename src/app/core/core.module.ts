e91c7b49f005fe5b2fb973726812876f9a72ea61
Bruno Rezende <brunoc.rez@gmail.com>
2020-05-30 08:40:52 -0300

    request interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
     declarations: [HeaderComponent],
@@ -10,6 +12,13 @@ import { HeaderComponent } from './header/header.component';
         CommonModule,
         RouterModule
     ],
-    exports: [HeaderComponent]
+    exports: [HeaderComponent],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true
+        }
+    ]
 })
 export class CoreModule { }
